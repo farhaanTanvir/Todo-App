f03ddf06d0f8077d1c4bc7d9557bb40a5d5d5319
Farhan Tanvir <ftanvir723@gmail.com>
2026-09-11 18:21:28 +0600

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: {
+        app: './src/index.js',
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            }
+        ],
+    }
+};
